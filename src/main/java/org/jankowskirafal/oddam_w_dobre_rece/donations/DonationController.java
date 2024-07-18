790d841d85972b381328080716ea38945b97583d
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-07-19 00:36:00 +0200

    Config
    added:
    repositories
    services
    controllers

---
+++ src/main/java/org/jankowskirafal/oddam_w_dobre_rece/donations/DonationController.java
@@ -0,0 +1,9 @@
+package org.jankowskirafal.oddam_w_dobre_rece.donations;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class DonationController {
+}
--- src/main/java/org/jankowskirafal/oddam_w_dobre_rece/donations/DonationService.java
+++ src/main/java/org/jankowskirafal/oddam_w_dobre_rece/donations/DonationService.java
@@ -1,4 +1,4 @@
-package org.jankowskirafal.oddam_w_dobre_rece.categories;
+package org.jankowskirafal.oddam_w_dobre_rece.donations;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 @Transactional
-public class CategoryService {
+public class DonationService {
 }
