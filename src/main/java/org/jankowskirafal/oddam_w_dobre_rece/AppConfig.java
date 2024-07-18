ef697795a61fe2caa7c2781305861be9b563f8a7
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-07-19 00:49:02 +0200

    Update AppConfig.java

--- src/main/java/org/jankowskirafal/oddam_w_dobre_rece/AppConfig.java
+++ src/main/java/org/jankowskirafal/oddam_w_dobre_rece/AppConfig.java
@@ -1,5 +1,9 @@
 package org.jankowskirafal.oddam_w_dobre_rece;
 
+import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 @Transactional
 public class AppConfig implements WebMvcConfigurer {
