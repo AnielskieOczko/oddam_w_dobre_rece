7d87d3d6ca6b6b9d17e4a056f65f52a5c2a4059f
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-07-27 13:57:27 +0200

    authentication setup

--- src/main/java/org/jankowskirafal/oddamwdobrerece/users/AuthorityRepository.java
+++ src/main/java/org/jankowskirafal/oddamwdobrerece/users/AuthorityRepository.java
@@ -1,13 +1,13 @@
 package org.jankowskirafal.oddamwdobrerece.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
 
-    Optional<User> findUserByEmail(String email);
-
+    Optional<Authority> findByName(String name);
 }
---
+++ src/main/java/org/jankowskirafal/oddamwdobrerece/users/UserPrincipal.java
@@ -0,0 +1,60 @@
+package org.jankowskirafal.oddamwdobrerece.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class UserPrincipal implements UserDetails {
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(Long id, String username, String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrincipal build(User user) {
+        List<GrantedAuthority> authorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
+
+        return new UserPrincipal(
+                user.getId(),
+                user.getEmail(), // Use email as the username
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+
+}
