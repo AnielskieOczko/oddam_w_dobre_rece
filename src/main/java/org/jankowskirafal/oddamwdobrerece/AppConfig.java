2dbdeebe0a9a8940644672e8cf5554ae5b0b1c29
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-09-30 22:23:20 +0200

    setup for validation messages
    added setup to store validation messages in one file

--- src/main/java/org/jankowskirafal/oddamwdobrerece/AppConfig.java
+++ src/main/java/org/jankowskirafal/oddamwdobrerece/AppConfig.java
@@ -3,7 +3,10 @@ package org.jankowskirafal.oddamwdobrerece;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.jankowskirafal.oddamwdobrerece.users.AuthorityConverter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +20,15 @@ public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(authorityConverter);
+    }
+
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages"); // Base name of your properties files
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+
     }
 }
